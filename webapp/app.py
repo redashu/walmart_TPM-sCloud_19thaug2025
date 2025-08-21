96c9163897b89f55a8aca28082b9dff07bc682c8
Ashutoshh <ashutoshh@linux.com>
2025-08-21 17:45:01 +0530

    updates

--- webapp/app.py
+++ webapp/app.py
@@ -8,34 +8,38 @@ app = Flask(__name__)
 BUCKET_NAME = "testpocnew1"
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "storae.json"
 
+# Initialize GCS client
 client = storage.Client()
 bucket = client.bucket(BUCKET_NAME)
 
-@app.route('/')
+@app.route("/")
 def index():
-    # List all files in GCS bucket
+    """List all files in the GCS bucket"""
     blobs = bucket.list_blobs()
     files = [blob.name for blob in blobs]
     return render_template("index.html", files=files)
 
-@app.route('/upload', methods=['POST'])
-def upload():
-    if 'file' not in request.files:
-        return "No file part"
+@app.route("/upload", methods=["POST"])
+def upload_file():
+    """Upload a file to GCS"""
+    if "file" not in request.files:
+        return "No file uploaded", 400
     
-    file = request.files['file']
-    if file.filename == '':
-        return "No selected file"
-
+    file = request.files["file"]
+    if file.filename == "":
+        return "No selected file", 400
+    
     blob = bucket.blob(file.filename)
-    blob.upload_from_file(file, content_type=file.content_type)
+    blob.upload_from_file(file)
 
-    return redirect(url_for('index'))
+    return redirect(url_for("index"))
 
-@app.route('/download/<filename>')
-def download(filename):
+@app.route("/view/<filename>")
+def view_file(filename):
+    """Generate a public URL for file"""
     blob = bucket.blob(filename)
-    return blob.download_as_bytes()
+    url = blob.generate_signed_url(version="v4", expiration=3600, method="GET")
+    return redirect(url)
 
 if __name__ == "__main__":
     app.run(debug=True)
